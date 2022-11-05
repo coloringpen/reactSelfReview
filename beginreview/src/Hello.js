d6eaf517724d68c8eee76057f6b973e68014dc60
Shirley <illangayo00@gmail.com>
2022-11-06 00:48:01 +0900

    조건부 렌더링 완료

--- beginreview/src/Hello.js
+++ beginreview/src/Hello.js
@@ -3,8 +3,30 @@ import React from 'react';
 function Hello(props) {
   //  return <div style={{ color: props.color }}> 안녕하세요 {props.name}</div>; // props는 전부 합쳐서 객체형태로 들어간다!
   // 비구조화 할당 활용하기
-  const { name, color } = props;
-  return <div style={{ color }}> 안녕하세요{name}</div>;
+  const { name, color, isSpecial } = props;
+  /*const star = props.isSpecial ? '*' : null;
+  return (
+    <div style={{ color }}>
+      {' '}
+      {star}안녕하세요{name}
+    </div>
+  );*/
+  /* return (
+    <div style={{ color }}>
+      {props.isSpecial ? '*' : null}안녕하세요{name}
+    </div>
+  );*/
+  /* return (
+    <div style={{ color }}>
+      {isSpecial ? <b>*</b> : null}안녕하세요{name}
+    </div>
+  );*/
+  // jsx도 값처럼 쓸 수 있다는 것을 기억하기
+  return (
+    <div style={color}>
+      {isSpecial ?? <b>*</b>}안녕하세요{name} {/* 첫번째 false반환하는 ?? */}
+    </div>
+  );
 }
 
 Hello.defaultProps = {
