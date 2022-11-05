91546959d297c52e03251fba34dc4e506e093341
Shirley <illangayo00@gmail.com>
2022-11-06 00:56:31 +0900

    useState 공부 시작

--- beginreview/src/App.js
+++ beginreview/src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import './App.css';
+import Counter from './Counter';
 
 function App() {
-  return (
-    <Wrapper>
-      {' '}
-      {/** 컴포넌트도 열리는 태그와 닫히는 태그로 구성될 수 있음
-       * 이럴 때는 하위, 자식 태그들이 보이게 할지, 말지 잘 처리할 것
-       */}
-      <Hello name="react" color="red" isSpecial />{' '}
-      {/* 아무런 값을 넣어주지 않을때, true로 값이 들어가는 props */}
-      <Hello color="pink" />
-    </Wrapper>
-  );
+  return <Counter />;
 }
 
 export default App;
