ad24e22091c95c9f560e4d1d7551b02415424b85
Shirley <illangayo00@gmail.com>
2022-11-07 02:12:49 +0900

    App.js 수정 추가

--- beginreview/src/App.js
+++ beginreview/src/App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import CreateUser from './CreateUser';
+import UserList from './UserList';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ]);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const nextId = useRef(4);
+
+  const onSubmit = () => {
+    const newUser = { id: nextId.current, username, email };
+    setUsers([...users, newUser]);
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id)); // 이 조건에 맞는 애들만 새배열의 요소가 될 수 있음
+  };
+
+  // 상태가 두 군데라는 것을 기억하기
+  // input 부분
+  // 리스트로 렌더링되는 배열 부분
+
   return (
-    <Wrapper>
-      {' '}
-      {/** 컴포넌트도 열리는 태그와 닫히는 태그로 구성될 수 있음
-       * 이럴 때는 하위, 자식 태그들이 보이게 할지, 말지 잘 처리할 것
-       */}
-      <Hello name="react" color="red" isSpecial />{' '}
-      {/* 아무런 값을 넣어주지 않을때, true로 값이 들어가는 props */}
-      <Hello color="pink" />
-    </Wrapper>
+    <div>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />
+      <UserList users={users} onRemove={onRemove} />
+    </div>
   );
 }
 
